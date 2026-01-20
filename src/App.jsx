33b7d3c82b5ce920ae2d0e94c53b55161128b76c
Felipe Makarios <comoquefazmarketing@gmail.com>
2026-01-20 00:21:29 -0300

    Fix: Importando RegisterSupplier e unificando rotas no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 ﻿import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// Domínios Organizados (Arquitetura de Empresa)
 import Home from "./domains/commercial/Home";
-import SpaceDetails from "./pages/SpaceDetails";
-import MySpaces from "./pages/MySpaces";
 import Dashboard from "./domains/backoffice/Dashboard";
 import Admin from "./domains/backoffice/Admin";
 import ChatWithLia from "./domains/support/ChatWithLia";
-function App() {
+
+// Componentes que ainda estão na pasta antiga (precisamos importar o RegisterSupplier)
+import SpaceDetails from "./pages/SpaceDetails";
+import MySpaces from "./pages/MySpaces";
+import RegisterSupplier from "./pages/RegisterSupplier"; 
+
+// Componentes Globais
+import MandaLaBanner from "./components/MandaLaBanner";
+
+export default function App() {
   return (
     <Router>
-      <div className="min-h-screen relative bg-[#FDFDFD]">
+      <div className="min-h-screen bg-white">
         <Routes>
-          {/* Feed Principal */}
           <Route path="/" element={<Home />} />
-          
-          {/* Rota Original e Rota que o seu Feed está chamando (/space/) */}
-          <Route path="/details/:id" element={<SpaceDetails />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/chat" element={<ChatWithLia />} />
           <Route path="/space/:id" element={<SpaceDetails />} />
-          
-          {/* Gestão e Cadastro */}
-          <Route path="/my-spaces" element={<MySpaces />} />
           <Route path="/register-supplier" element={<RegisterSupplier />} />
-
-          {/* Redirecionar qualquer erro para a Home */}
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-
+        
+        {/* Banner Único e Conectado - Sensorial Tum Dum */}
         <MandaLaBanner />
       </div>
     </Router>
   );
 }
-
-export default App;
