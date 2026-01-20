e4fff2a02d6cd5cad4653cabb2c6976ce864e23c
Felipe Makarios <comoquefazmarketing@gmail.com>
2026-01-20 01:07:39 -0300

    Fix: Removendo caracteres invisíveis (BOM) que travam o build

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Calculator, Store, Menu, X, Home } from 'lucide-react';
 
 export default function Sidebar() {
